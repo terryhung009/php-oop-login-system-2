98e9f965170274b4dbe05f564ac5ac31edd29276
danny <khc741213@gmail.com>
2022-09-29 18:30:46 +0800

    1

--- classes/signup.classes.php
+++ classes/signup.classes.php
@@ -2,20 +2,6 @@
 
 class Signup extends Dbh{
 
-  protected function setUser($uid, $pwd, $email){
-    $statement =  $this->connect()->preapre('INSERT INTO users (users_uid, users_pwd, users_email) VALUES (?, ? , ?);');
-
-    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-
-    if(!$statement->execute(array($uid, $pwd, $email))){
-      $statement = null;
-      header("location: ../index.php?error=statementfailed");
-      exit();
-    }
-
-    $statement = null;
-  }
-
   protected function checkUser($uid, $email){
     $statement =  $this->connect()->preapre('SELECT users_uid FROM user where users_uid = ? OR users_email = ?;');
 
@@ -35,6 +21,24 @@ class Signup extends Dbh{
     }
 
     return $resultCheck;
+  }
+
+
+
+  protected function setUser($uid, $pwd, $email){
+
+    // $statement = $this->connect()->prepare('SELECT users_uid FROM users WHERE users_uid = ? OR users_email = ?;');
+    $statement =  $this->connect()->preapre('INSERT INTO users (users_uid, users_pwd, users_email) VALUES (?, ? , ?);');
+
+    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+    if(!$statement->execute(array($uid, $pwd, $email))){
+      $statement = null;
+      header("location: ../index.php?error=statementfailed");
+      exit();
+    }
+
+    $statement = null;
   }
 
 }
